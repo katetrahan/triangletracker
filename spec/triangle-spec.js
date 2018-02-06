fe5495f1377515ce8357709cc76aabff4e4856d6
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-06 11:15:08 -0800

    add karma, run tests

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,6 +1,15 @@
 var Triangle = require('./../js/triangle.js').triangleModule;
 
 describe('Triangle', function() {
+  var reusableTriangle;
+
+  beforeEach(function() {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
+
+  it('should show how beforeEach() works', function() {
+    console.log(reusableTriangle);
+  });
 
   it('should test whether a Triangle has three sides', function() {
     var triangle = new Triangle(3,4,5)
